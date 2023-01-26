a544957cb7191995dc7b8ddca981c819123b2a57
vanessabou <boufassa.vanessa@gmail.com>
2023-01-26 22:22:07 +0100

    commit frontend part all pages

--- frontend/src/components/userHome.js
+++ frontend/src/components/userHome.js
@@ -7,8 +7,8 @@ export default function UserHome({ userData }) {
   };
   return (
     <div>
-      Name: <h1>{userData.fname}</h1>
-      Email: <h1>{userData.email}</h1>
+      <h1>{userData.fname}</h1>
+      <h1>{userData.email}</h1>
       <br />
       <button onClick={logOut} className="btn btn-primary">
         Log Out
